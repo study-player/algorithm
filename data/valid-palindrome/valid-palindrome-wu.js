508f188cef389bda45ec17f68dd7e01565bb1caa
wubohong <wubohong@codemao.cn>
2018-10-07 14:48:29 +0800

    feat: add a solved question named valid-palindrome

---
+++ data/valid-palindrome/valid-palindrome-wu.js
@@ -0,0 +1,35 @@
+let isPalindrome = s => {
+  if (s.length === 0) {
+    return true;
+  }
+  s = s.toLowerCase().replace(/[^a-z0-9]/g, '');
+  let half = Math.floor(s.length / 2);
+  let flag = 0;
+  for (let i = 0; i < half; i++) {
+    if (s.charAt(i) !== s.charAt(s.length - i - 1)) {
+      flag = 1;
+      break;
+    }
+  }
+  if (flag === 1) {
+    return false;
+  }
+  return true;
+}
+
+let isPalindrome2 = s => {
+  s = s.replace(/[^a-zA-Z]/gi, '').toLowerCase();
+  let _s = s.split('').reverse().join('');
+  if (s === _s) {
+    return true;
+  }
+  return false;
+}
+
+let s = 'A man, a plan, a canal: Panama';
+console.log(isPalindrome2(s));
+
+/**
+ * https://leetcode-cn.com/problems/valid-palindrome/description/
+ * 在本地，以及 leetcode 的 playground 调试都没问题，但是过不了
+ */
